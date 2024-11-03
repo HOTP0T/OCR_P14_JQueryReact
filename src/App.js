5a86d89cb08d19cc0c731abafe8c537716fef150
HOTPOT <tpcmaky@gmail.com>
2024-11-03 04:04:58 +0100

    changed png to webp for better perf results

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <EmployeeProvider>
       <Router>
         <div className="title">
-          <img src="https://user.oc-static.com/upload/2024/02/21/17085055322514_Capture%20d%E2%80%99e%CC%81cran%202024-02-21%20a%CC%80%2009.52.02.png" alt="Company Logo" />
+          <img src="/images/logo_WealthHealth.webp" alt="Company Logo" />
           <h1>HRnet</h1>
         </div>
         <nav>
